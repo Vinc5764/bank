7b93559fe822dde65e881b84aa9e48c9af0c73ba
Vinc5764 <vincentdanso05@gmail.com>
2024-08-04 20:00:07 +0000

    responsiveness

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import Donation from "../api/model/donation.model";
 import Header1 from "../components/Header1";
 import Footer from "../components/Footer";
+import DonationForm from "../components/DonationForm";
 
 const page = () => {
   return (
     <div>
       <Header1 />
-      <Donation />
+      <DonationForm />
       <Footer />
     </div>
   );
