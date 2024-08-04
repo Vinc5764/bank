740621872790222e2fd2dd1239ad1c23454bef67
Vinc5764 <vincentdanso05@gmail.com>
2024-08-04 17:02:48 +0000

    update stuff

--- app/components/UserDash.tsx
+++ app/components/UserDash.tsx
@@ -64,7 +64,7 @@ export default function UserDash() {
     };
 
     fetchMembers();
-  }, [url, datas._id]); // Add dependencies
+  }, [datas._id]); // Add dependencies
 
   const handleInputChange = (e: any) => {
     const { id, value } = e.target;
